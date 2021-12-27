d27089e040a9c3d86c79ba5266584280336680dd
Karthik Ruthiravelu <karthik19894@gmail.com>
2021-12-27 13:39:21 +0530

    Time: 100 ms (51.88%), Space: 41.3 MB (92.90%) - LeetHub

--- basic-calculator-ii/basic-calculator-ii.js
+++ basic-calculator-ii/basic-calculator-ii.js
@@ -5,34 +5,28 @@
 var calculate = function(s) {
     let operation = "+";
     let number = 0;
-    const stack = [];
+    let lastNumber = 0;
+    let result = 0;
     for(let i=0; i < s.length; i++){
         let char = s[i];
         if(isDigit(char)){
             number = (number * 10) + (char - "0");
         }
         if(isOperator(char) || i===s.length-1){
-            if(operation === "+"){
-                stack.push(number);
-            }else if(operation === "-"){
-                stack.push(-number);
+            if(operation === "+" || operation === "-"){
+                result += lastNumber;
+                lastNumber = operation === "+" ? number : -number;
             }else if(operation === "*"){
-                let stackTop = stack.pop();
-                stack.push(stackTop * number);
+                lastNumber = number * lastNumber;
             }else if(operation === "/"){
-                let stackTop = stack.pop();
                 // floor on negative number will result in wrong calculation for eg: floor of -3/2 will give -2 as that is the smallest number closer to -1.5 as -1 will be greater than -1.5
-                stack.push(Math.trunc(stackTop / number));
+                lastNumber = Math.trunc(lastNumber / number);
             }
             number = 0;
             operation = char;
         }
     }
-    let result = 0;
-    while(stack.length){
-        const num = stack.pop();
-        result += num;
-    }
+    result += lastNumber;
     return result;
 };
 
