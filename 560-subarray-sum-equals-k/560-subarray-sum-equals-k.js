07a23c883e064102c3e034cb6ec2ab42e69961b5
Karthik Ruthiravelu <karthik19894@gmail.com>
2022-02-10 10:06:58 +0530

    Time: 116 ms (71.73%), Space: 53.7 MB (12.18%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+    const prefixSumMap = {};
+    let subArrayCount = 0;
+    let sum = 0;
+    for(let num of nums){
+        sum += num;
+        if(sum === k){
+            subArrayCount++;
+        }
+        if((sum - k) in prefixSumMap){
+            subArrayCount += prefixSumMap[sum - k];
+        }
+        if(!(sum in prefixSumMap)) prefixSumMap[sum] = 0;
+        prefixSumMap[sum] += 1;
+    }
+    return subArrayCount;
+};
