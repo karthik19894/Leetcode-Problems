89cbb65726a297d760db1dba7b0929a1228c3e2b
Karthik Ruthiravelu <karthik19894@gmail.com>
2022-02-27 13:38:21 +0530

    Time: 156 ms (16.21%), Space: 48.2 MB (23.65%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.js
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.js
@@ -13,17 +13,16 @@
 var widthOfBinaryTree = function(root) {
     if(root === null) return 0;
     let maxWidth = 0;
-    const queue = [];
-    queue.push([root, 0]);
-    while(queue.length > 0){
-        let firstNodeInQueue = queue[0];
+    const queue = new Queue();
+    queue.enqueue([root, 0]);
+    while(queue.size() > 0){
+        let firstNodeInQueue = queue.front();
         const [_, idxOfFirstNode] = firstNodeInQueue;
-        const reset = queue.length === 1;
-        let length = queue.length;
+        let length = queue.size();
         for(let i=0; i < length; i++){
-            const [nextNode, nextIdx] = queue.shift();
-            if(nextNode.left) queue.push([nextNode.left, (nextIdx * 2) - 1]);
-            if(nextNode.right) queue.push([nextNode.right, (nextIdx * 2)]);
+            const [nextNode, nextIdx] = queue.dequeue();
+            if(nextNode.left) queue.enqueue([nextNode.left, (nextIdx * 2) - 1]);
+            if(nextNode.right) queue.enqueue([nextNode.right, (nextIdx * 2)]);
             let currentWidth = (nextIdx - idxOfFirstNode) + 1;
             if(currentWidth){
                 maxWidth = Math.max(maxWidth, currentWidth);
