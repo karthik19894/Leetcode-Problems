35af2b8d0839b087e0882b19c4a16a47764091f8
Karthik Ruthiravelu <karthik19894@gmail.com>
2022-03-12 20:52:23 +0530

    Time: 79 ms (66.42%), Space: 44.2 MB (37.73%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.js
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.js
@@ -13,29 +13,16 @@
  */
 var copyRandomList = function(head) {
     if(head === null) return null;
-    populateIndices(head);
-    const cache = {};
+    const cache = new WeakMap();
     return copyList(head, cache);
 };
 
 function copyList(head, cache){
     if(head === null) return null;
-    const key = `${head.val}:${head.index}`;
-    if(key in cache) return cache[key];
+    if(cache.has(head)) return cache.get(head);
     const newHead = new Node(head.val, null, null);
-    newHead.index = head.index;
-    cache[key] = newHead;
+    cache.set(head, newHead);
     newHead.next = copyList(head.next, cache);
     newHead.random = copyList(head.random, cache);
     return newHead;
-}
-
-function populateIndices(head){
-    let current = head;
-    let idx = 0;
-    while(current !== null){
-        current.index = idx;
-        idx++;
-        current = current.next;
-    }
 }
